c58b0051acd2f2fecdfac7b3336615d869d5a38b
jinyeoung <dkdlel7090@naver.com>
2021-10-05 21:20:22 +0900

    theme 수정, GlobalStyle 생성 (dark모드)

---
+++ src/theme/Theme.js
@@ -0,0 +1,20 @@
+
+
+export const LightTheme = {
+    iconColor : '#4F4E4E',
+    bgColor: '#FFFFFF',
+    textColor: '#000000',
+    borderColor: '1px solid #eaeaea',
+};
+
+export const DarkTheme = {
+    iconColor : '#FFFFFF',
+    bgColor: '#000000',
+    textColor: '#FFFFFF',
+    borderColor: '1px solid #2c2d33',
+};
+
+export const theme = {
+    LightTheme,
+    DarkTheme,
+};
