96bddca97c7d1f18e61fd60c01aa9d98a0018810
mookbap <mookbap@naver.com>
2021-10-31 14:03:34 +0900

    WriteMemo save load
    async storage 사용한 save load

--- src/screens/WriteMemo.js
+++ src/screens/WriteMemo.js
@@ -1,15 +1,46 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Button, SpeedDial,ListItem } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function WriteMemo() {
 
-    const [txt, selTxt] = useState('Hello world!')
+    const [txt, setTxt] = useState('Hello world!')
     const navigation = useNavigation();
+
+    useEffect(()=> {
+        console.log('시작!')
+        loadData(); //이 스크린 시작할때 실행시킴
+    },[]);
+
+
+    const saveData = async (value) => {
+        try {
+          await AsyncStorage.setItem('memong', value)
+          console.log('저장완료')
+        } catch (e) {
+          // saving error
+        }
+      }
+
+    
+    const loadData = async () => {
+        try {
+        const value = await AsyncStorage.getItem('memong')
+        if(value !== null) {
+            setTxt(value);
+            console.log('불러오기 완료')
+
+            // value previously stored
+        }
+        } catch(e) {
+        // error reading value
+        }
+    }
 
 
     return(
@@ -18,13 +49,14 @@ export default function WriteMemo() {
                 <StatusBar style="auto" />
                     <View style={{padding:10, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
                         <Button
-                        titleStyle={{
-                            color: "red",
-                            fontSize: 23,}}
-                        onPressOut={()=>navigation.navigate('Home')}
-                        type="clear"
-                        icon={<Icon name="chevron-left" size={30} color="#4F4E4E"/>}
-                        title="  메모제목"/> 
+                            onPress={()=>saveData(txt)}
+                            titleStyle={{
+                                color: "red",
+                                fontSize: 23,}}
+                            onPressOut={()=>navigation.navigate('Home')}
+                            type="clear"
+                            icon={<Icon name="chevron-left" size={30} color="#4F4E4E"/>}
+                            title="  메모제목"/> 
 
                         <Button
                             type="clear"
@@ -32,10 +64,10 @@ export default function WriteMemo() {
                             icon={<Icon name="ellipsis-v" size={23} color="#4F4E4E"/>}
                         />                          
                     </View>
-                    <View style={{backgroundColor:'#eeeee', flex:1, padding:10}}>
+                    <View style={{backgroundColor:'#eeeeee', flex:1, padding:10}}>
                         <TextInput 
                             value={txt}
-                            onChangeText={txt => selTxt(txt)}
+                            onChangeText={txt => setTxt(txt)}
                             multiline
                         />
                     </View>
